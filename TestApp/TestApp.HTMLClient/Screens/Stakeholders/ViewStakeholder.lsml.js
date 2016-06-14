dbf8d9fe57b05d79ed2611d0d5ecffda6cfef6cd
cj5194 <cj5194@W01344>
2016-06-14 09:31:41 +0100

    Add project files.

---
+++ TestApp/TestApp.HTMLClient/Screens/Stakeholders/ViewStakeholder.lsml.js
@@ -0,0 +1,10 @@
+﻿/// <reference path="~/GeneratedArtifacts/viewModel.js" />
+
+myapp.ViewStakeholder.Details_postRender = function (element, contentItem) {
+    // Write code here.
+    var name = contentItem.screen.Tbl_Stkhld_Datum.details.getModel()[':@SummaryProperty'].property.name;
+    contentItem.dataBind("screen.Tbl_Stkhld_Datum." + name, function (value) {
+        contentItem.screen.details.displayName = value;
+    });
+}
+
